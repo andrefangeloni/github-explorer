e6fe920b55ef896566d394ff347d3fbffe89815e
andrefangeloni <andrefangeloni@gmail.com>
2021-09-04 23:18:22 -0300

    feat: routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  );
-}
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Routes from "./routes";
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
