d4f6df65fe8e5e0874ed2be03c3bc910d3f73d94
andrefangeloni <andrefangeloni@gmail.com>
2021-09-05 00:21:16 -0300

    feat: styled dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no GitHub</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="GitHub Explorer" />
+    <Title>Explore repositórios no GitHub</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">
+        Pesquisar
+      </button>
+    </Form>
+
+    <Repositories>
+      <a href="https://github.com/">
+        <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy high scalable forms</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="https://github.com/">
+        <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy high scalable forms</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="https://github.com/">
+        <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy high scalable forms</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
