97771e5281af4af5f8ccf6c1a7ac4979fb93ab92
andrefangeloni <andrefangeloni@gmail.com>
2021-09-05 22:50:59 -0300

    feat: styled repository details

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -79,7 +80,13 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="https://github.com/">
+          <Link
+            key={repository.full_name}
+            to={{
+              pathname: '/repository',
+              state: repository.full_name,
+            }}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -91,7 +98,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,67 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface stateType {
+  from: {
+    pathname: string;
+  };
+}
+
+const Repository: React.FC = () => {
+  const { state } = useLocation<stateType>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://github.com/andrefangeloni.png" alt="Avatar" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>adasdsa</strong>
+            <p>asdasd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Repository;
